76ff2220aa4de2f2849ba2bb8e7c5b6aa5ea8710
JoseShota <eljosepromedio@gmail.com>
2025-12-09 20:30:34 -0600

    Implementar automatización del idioma

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+  i18n: {
+    defaultLocale: "es", // Tu idioma principal
+    locales: ["es", "en"], // Los idiomas disponibles
+    routing: {
+        prefixDefaultLocale: false // 'es' estará en la raíz (/), 'en' estará en (/en)
+    }
+  }
+});
---
+++ src/i18n/ui.ts
@@ -0,0 +1,22 @@
+// src/i18n/ui.ts
+export const languages = {
+  es: 'Español',
+  en: 'English',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+  es: {
+    'nav.home': '[Inicio]',
+    'nav.music': '[Música]',
+    'nav.videos': '[Videos]',
+    'nav.cv': '[CV/Contacto]',
+  },
+  en: {
+    'nav.home': '[Home]',
+    'nav.music': '[Music]',
+    'nav.videos': '[Videos]',
+    'nav.cv': '[CV/Contact]',
+  },
+};
