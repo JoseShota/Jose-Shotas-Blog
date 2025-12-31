fce409ecc8497934906e10447198c5c4685afbb4
JoseShota <eljosepromedio@gmail.com>
2025-12-31 17:14:01 -0500

    Edición de la sección curaduría

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,12 @@ import react from '@astrojs/react';
 
 // Mantenemos la lógica de detección, es la mejor forma
 const isGitHub = process.env.GITHUB_ACTIONS === 'true';
+import tailwindcss from "@tailwindcss/vite";
+
 
 export default defineConfig({
   site: 'https://JoseShota.github.io',
-
-  // CORRECCIÓN AQUÍ:
-  // Usa EXACTAMENTE el nombre que vimos en tu captura (respetando mayúsculas/minúsculas)
   base: isGitHub ? '/Jose-Shotas-Blog' : '/',
-
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
@@ -19,10 +17,11 @@ export default defineConfig({
         prefixDefaultLocale: false
     }
   },
-
   build: {
     format: 'directory'
   },
-
-  integrations: [react()]
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
